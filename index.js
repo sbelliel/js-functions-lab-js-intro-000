297cc42842b4e494ffa1882984102617b743c9a9
Sam Belliel <sbelliel@gmail.com>
2018-07-14 02:20:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function happyHolidays() {
   return "Happy holidays!"
-}// write your code below!
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}!`
+}
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
